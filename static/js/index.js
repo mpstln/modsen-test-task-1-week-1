7b3dbaa2751f4bf7467c80a11a54a825b70869bb
mpstln <vans14@list.ru>
2024-02-14 00:39:48 +0300

    1.1.0

--- static/js/index.js
+++ static/js/index.js
@@ -6,7 +6,7 @@ questions = {
     "What is the capital of France?": ["Paris", "London", "Berlin", "Rome"],
     "What is the name of the largest desert in the world?": ["Sahara", "Amazon", "Gobi", "Kalimantan"],
     "What is the name of the smallest bird in the world?": ["Hummingbird", "Ostrich", "Penguin", "Flamingo"],
-    "Which language is used for web development?": ["JavaScript", "Python", "Java", "C++"],
+    "Which language is used for web development?": ["JavaScript", "Python", "Java", "C"],
     "In which year was the company Google founded?": ["1998", "2005", "1995", "2000"],
     "Which fruit is a source of vitamin C?": ["Orange", "Apple", "Banana", "Watermelon"],
     "Which gas is the main component of the air?": ["Nitrogen", "Carbon dioxide", "Hydrogen", "Oxygen"],
