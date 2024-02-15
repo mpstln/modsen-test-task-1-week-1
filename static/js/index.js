996f1c0250968679c370d4f2ec7ba0a655127c3e
mpstln <vans14@list.ru>
2024-02-15 04:46:44 +0300

    '1.1.1'

--- static/js/index.js
+++ static/js/index.js
@@ -1,81 +1,205 @@
-const root = document.documentElement;
-
-
-let clickCount = 0;
-questions = {
-    "What is the capital of France?": ["Paris", "London", "Berlin", "Rome"],
-    "What is the name of the largest desert in the world?": ["Sahara", "Amazon", "Gobi", "Kalimantan"],
-    "What is the name of the smallest bird in the world?": ["Hummingbird", "Ostrich", "Penguin", "Flamingo"],
-    "Which language is used for web development?": ["JavaScript", "Python", "Java", "C"],
-    "In which year was the company Google founded?": ["1998", "2005", "1995", "2000"],
-    "Which fruit is a source of vitamin C?": ["Orange", "Apple", "Banana", "Watermelon"],
-    "Which gas is the main component of the air?": ["Nitrogen", "Carbon dioxide", "Hydrogen", "Oxygen"],
-    "Which sport is associated with the use of a snowboard?": ["Snowboarding", "Soccer", "Tennis", "Volleyball"]
-}
-
-//jumbled questions
-let randomQuestions = {};
-for (let key of Object.keys(questions).sort(() => Math.random() - 0.5)){
-    randomQuestions[key] = [questions[key].slice().sort(() => Math.random() - 0.5), questions[key][0]];
-}
-let keys = Object.keys(randomQuestions);
-let answer = randomQuestions[keys[0]][1];
-let answers = randomQuestions[keys[0]][0];
-let questionsNumber = Object.keys(questions).length;
-
-let buttonsId = ['button1', 'button2', 'button3', 'button4'].map((button) => {
-    return document.getElementById(button);
-});
-
-let score = 0;
-
-function changeText(){
-    document.getElementById('question').innerText = `Question ${(clickCount+1).toString()}/${questionsNumber}`;
-    document.getElementById('text').innerText = `${keys[clickCount]}`;
-   
-    for(let i = 0; i < buttonsId.length; i++) {
-        buttonsId[i].innerText = randomQuestions[keys[clickCount]][0][i];
+let questions = [
+    {
+        question: "What is the capital of France?",
+        answers: ["Paris", "London", "Berlin", "Rome"],
+        answer: "Paris",
+    },
+    {
+        question: "What is the name of the largest desert in the world?",
+        answers: ["Sahara", "Amazon", "Gobi", "Kalimantan"],
+        timer: 10000,
+        answer: "Sahara",
+    },
+    {
+        question: "What is the name of the smallest bird in the world?",
+        answers: ["Hummingbird", "Ostrich", "Penguin", "Flamingo"],
+        answer: "Hummingbird",
+    },
+    {
+        question: "Which language is used for web development?",
+        answers: ["JavaScript", "Python", "TypeScript", "C"],
+        responsesNumber: 2,
+        answer: ["JavaScript", "TypeScript"],
+    },
+    {
+        question: "In which year was the company Google founded?",
+        answers: ["1998", "2005", "1995", "2000"],
+        answer: "1998",
+    },
+    {
+        question: "Which fruit is a source of vitamin C?",
+        answers: ["Orange", "Apple", "Banana", "Watermelon"],
+        answer: "Orange",
+    },
+    {
+        question: "Which gas is the main component of the air?",
+        answers: ["Nitrogen", "Carbon dioxide", "Hydrogen", "Oxygen"],
+        answer: "Nitrogen",
+    },
+    {
+        question: "Which sport is associated with the use of a snowboard?",
+        answers: ["Snowboarding", "Soccer", "Tennis", "Volleyball"],
+        answer: "Snowboarding",
     }
+];
 
-    answer = randomQuestions[keys[clickCount]][1];
-    answers = randomQuestions[keys[clickCount]][0];
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-    changeText()
-});
+class Quiz {
+    constructor(questions) {
+        this.questions = questions;
+        this.randomQuestions = [];
+        for (let i = 0; i < questions.length; i++) {
+            this.randomQuestions.push(i);
+        }
+        this.randomQuestions = this.randomizeArray(this.randomQuestions);
 
-function clickButton(event) {
-    clickCount++;
-
-    if (event.target.innerText === answer) {
-        score++;
-    }
-
-    buttonsId.forEach(button => {
-        button.style.backgroundColor = 'red';
-        button.disabled = true;
-    });
-    buttonsId[answers.indexOf(answer)].style.backgroundColor = 'green';
+        this.buttonsId = ['button1', 'button2', 'button3', 'button4'].map((button) => {
+            return document.getElementById(button);
+        });
+        this.questionId = document.getElementById('question');
+        this.textId = document.getElementById('text');
+        this.warningId = document.getElementById('warning');
         
-    setTimeout(() => {
-        buttonsId.forEach(button => {
-            button.style.backgroundColor = '';
-            button.disabled = false;
+        this.questionNumber = 0;
+        this.score = 0;
+
+        this.userAnswer = [];
+        this.events = [];
+        this.timer;
+    }
+    start(event){
+        this.userAnswer.push(event.innerText);
+        this.events.push(event);
+        event.style.backgroundColor = '#2a9d8f';
+        event.disabled = true;
+        if (this.userAnswer.length >= this.checkNumberOfAnswers()) {
+            for (let i of this.events) {
+                i.style.backgroundColor = '';
+                i.disabled = false;
+            }
+            if (this.timer) {
+                clearTimeout(this.timer);
+                this.timer = null;
+            }
+            this.end(this.userAnswer);
+            this.userAnswer = [];
+            this.events = [];
+        }
+    }
+    end(userAnswer) {
+        if (this.checkAnswer(userAnswer)) {
+            this.score++;
+        }
+        this.buttonsId.forEach(button => {
+            button.style.backgroundColor = '#8a2424';
+            button.disabled = true;
+        });
+        let answerButtons = this.answerButtons();
+        console.log(answerButtons);
+        answerButtons.forEach(button => {
+            button.style.backgroundColor = '#186118';
         });
 
-        if ((clickCount+1) > questionsNumber) {
-            document.getElementById('content').innerHTML = `<div id="result"> Your result: ${score}/${questionsNumber} </div> <button id="buttonReboot"> Restart test </button>`;
-            document.getElementById('buttonReboot').addEventListener('click', function() {
-                location.reload();
+        setTimeout(() => {
+            this.buttonsId.forEach(button => {
+                button.style.backgroundColor = '';
+                button.disabled = false;
             });
-            return
+    
+            this.questionNumber++;
+            if (this.questionNumber >= this.randomQuestions.length) {
+                this.results();
+                return
+            }
+            this.updateQuestion();
+        }, 1000);
+    }
+    questionWithTimer(time) {
+        if (!this.timer) {
+            if (time) {
+                this.timer = setTimeout(() => {
+                    this.end([]);
+                }, time);
+            }
         }
-        changeText()
-    }, 1000);
+    }
+    answerButtons(){
+        let answer = this.answerToArray(this.questions[this.randomQuestions[this.questionNumber]].answer);
+        let buttons  = [];
+        for (let button of this.buttonsId) {
+            if (answer.includes(button.textContent)) {
+                buttons.push(button);
+            }
+        }
+        return buttons;
+    }
+    checkNumberOfAnswers() {
+        if (this.questions[this.randomQuestions[this.questionNumber]].responsesNumber) {
+            return this.questions[this.randomQuestions[this.questionNumber]].responsesNumber;
+        }
+        return 1;
+    }
+    randomizeArray(array) {
+        return array.sort(() => Math.random()-0.5);
+    }
+    answerToArray(answer) {
+        return Array.isArray(answer) ? answer : [answer];
+    }
+    madeWarning()  {
+        let warning = '\u200B';
+        if (this.questions[this.randomQuestions[this.questionNumber]].timer) {
+            warning += `Time left: ${this.questions[this.randomQuestions[this.questionNumber]].timer/1000} seconds\n`;
+        }
+        if (this.questions[this.randomQuestions[this.questionNumber]].responsesNumber) {
+            warning += `Select ${this.checkNumberOfAnswers()} answers\n`;
+        }
+        return warning
+    }
+    updateQuestion() {
+        if (this.questions[this.randomQuestions[this.questionNumber]].timer) {
+            this.questionWithTimer(this.questions[this.randomQuestions[this.questionNumber]].timer);
+        }
+
+        this.questionId.innerText = `Question ${(this.questionNumber+1).toString()}/${this.randomQuestions.length}`;
+        this.textId.innerText = `${this.questions[this.randomQuestions[this.questionNumber]].question}`;
+        this.warningId.innerText = this.madeWarning();
+        
+        let answers = this.randomizeArray(this.questions[this.randomQuestions[this.questionNumber]].answers);
+        for(let i = 0; i < this.buttonsId.length; i++) {
+            this.buttonsId[i].innerText = answers[i];
+        }
+    }
+    checkAnswer(userAnswer) {
+        let answer = this.answerToArray(this.questions[this.randomQuestions[this.questionNumber]].answer);
+        if (!Array.isArray(userAnswer)) {
+            throw new Error('This is not an array');
+        }
+        if (userAnswer.length !== answer.length) {
+            return false;
+        }
+        const sortedUserAnswer = userAnswer.slice().sort();
+        const sortedAnswer = answer.slice().sort();
+        return sortedUserAnswer.every((value, index) => value === sortedAnswer[index]);
+    }
+    results() {
+        document.getElementById('content').innerHTML = `<div id="result"> Your result: ${this.score}/${this.randomQuestions.length} </div> <button id="buttonReboot"> Restart test </button>`;
+        document.getElementById('buttonReboot').addEventListener('click', function() {
+            location.reload();
+        });
+    }
+}
+
+//Quiz
+const quiz = new Quiz(questions);
+document.addEventListener('DOMContentLoaded', function() {
+    quiz.updateQuestion();
+});
+function clickButton(event) {
+    quiz.start(event.target);
 }
 
 // Night theme
+const root = document.documentElement;
 const themeToggleCheckbox = document.getElementById('themeToggleCheckbox');
 const savedTheme = localStorage.getItem('selectedTheme');
 
